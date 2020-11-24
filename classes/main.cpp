645a07084cf2a451352dd6e7e99276313fab1d19
gplopan <georgiana-andreea.plopan@student.tuiasi.ro>
2020-11-24 03:55:13 +0200

    cmake and main added

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <QApplication>
+#include <QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QHBoxLayout>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+
+    QWidget *window = new QWidget;
+    QPushButton *button1 = new QPushButton("One");
+    QPushButton *button2 = new QPushButton("Two");
+    QPushButton *button3 = new QPushButton("Three");
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(button1);
+    layout->addWidget(button2);
+    layout->addWidget(button3);
+    window->setLayout(layout);
+    window->show();
+    
+    return app.exec();
+
+    //insert event related instructions after this
 }
